589f5e7098dd53a780d249304259638e940fded9
BuzinAlexey <105777794+BuzinAlexey@users.noreply.github.com>
2022-05-26 10:42:02 +0300

    программа нахождения квадрата числа

---
+++ Program1.cs
@@ -0,0 +1,5 @@
+﻿// Пограмма вычисления квадрата числа
+    Console.Write("Введите число:");
+    int x = Convert.ToInt32(Console.ReadLine());    
+        int sqr = x * x;
+              Console.WriteLine("Квадрат числа равен:"+sqr);
